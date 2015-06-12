befcf8196d4347193672804f114e11260663d46d
Truman Zhou <truman.ck.zhou@gmail.com>
2015-06-12 19:53:21 +0800

    dev

--- dockernized/start.sh
+++ dockernized/start.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-docker run  -d -t  --name "as" -h "as"    trumanz/ubuntu12.04:dev  /bin/bash
-docker exec -t -i  as  wget http://public-repo-1.hortonworks.com/ambari/ubuntu12/2.x/updates/2.0.1/ambari.list
-docker exec -t -i  as  mv  ambari.list  /etc/apt/sources.list.d/
-docker exec -t -i  as  apt-key adv --recv-keys --keyserver keyserver.ubuntu.com B9733A7A07513CAD
-docker exec -t -i  as  apt-get  update
-docker exec -t -i  as  apt-get  install ambari-server
+docker run  -d -t  --name "as" -h "as"    trumanz/ambari:dev   /bin/bash
 docker exec -t -i  as  /etc/init.d/ssh restart
 
 
-docker run  -d -t  --name "ag1" -h "ag1"    trumanz/ubuntu12.04:dev  /bin/bash
+docker run  -d -t  --name "ag1" -h "ag1"  trumanz/ambari:dev     /bin/bash
 docker exec -t -i  ag1  /etc/init.d/ssh restart
 
-docker inspect -f  '{{.NetworkSettings.IPAddress}}' as
-docker inspect -f  '{{.NetworkSettings.IPAddress}}' ag1
+
+ASIP=$(docker inspect -f  '{{.NetworkSettings.IPAddress}}' as)
+AG1IP=$(docker inspect -f  '{{.NetworkSettings.IPAddress}}' ag1)
+
+docker exec -t -i  as   bash -c  "echo '$AG1IP  ag1'  >>   /etc/hosts"
+docker exec -t -i  ag1  bash -c  "echo '$ASIP  as'  >>   /etc/hosts "
+
+docker exec -t -i  as   ambari-server setup -s  -j /usr/lib/jvm/java-7-openjdk-amd64/  
+docker exec -t -i  as   ambari-server start 
+
+echo "Try http://$ASIP:8080 with admin:admin on firefox" 
